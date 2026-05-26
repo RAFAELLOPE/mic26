ed2918d2ef20975eabf28dcb3660dd28b9f89496
r.lopez <r.lopez@htime.org>
2026-05-26 20:22:27 +0200

    Save test results into a file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from sklearn.metrics import classification_report
 sys.path.append(os.path.abspath('.\\mic26'))
 from src.trainer.training import Experiment
 from config import Config
@@ -10,3 +11,16 @@ if __name__ == "__main__":
     config = Config()
     experiment = Experiment(config)
     results =experiment.run()
+    out_results = experiment.evaluate()
+    print("\n" + classification_report(
+        out_results['true'], 
+        out_results['prediction'], 
+        target_names=['Control (Spec)', 'PD (Sens)'])
+    )
+    # Save results to file
+    with open(os.path.join(config.out_dir, "test_results.txt"), "w") as f:
+        f.write(classification_report(
+            out_results['true'], 
+            out_results['prediction'], 
+            target_names=['Control (Spec)', 'PD (Sens)'])
+        )
